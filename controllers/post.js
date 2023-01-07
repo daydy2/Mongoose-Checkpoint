0c35340ce06598dcc8703d72f3ef2aa9c6fb48b6
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2023-01-07 18:39:32 +0100

    feat: added findOne

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Person = require("../models/Person");
 
-exports.postInfoPerson = async (req, res, next) => {
+//Create and Save a Record of a Model:
+exports.postInfoPerson = (req, res, next) => {
   console.log(req.body);
   const name = req.body.name;
   const age = req.body.age;
@@ -11,10 +12,64 @@ exports.postInfoPerson = async (req, res, next) => {
     age: age,
     favoritefood: favoriteFood,
   });
-  try {
-    const savedPost = await person.save();
-    res.json(savedPost);
-  } catch (err) {
-    res.status(400).json({ message: err });
-  }
+  person.save((err, data) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    } else {
+      res.status(200).json(data);
+    }
+  });
+};
+
+exports.getInfoPeople = (req, res, next) => {
+  res.status(200).send("Welcome to people's page");
+};
+
+//Create Many Records with model.create()
+exports.postInfoPeople = (req, res, next) => {
+  const arrayOfPeople = req.body;
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    } else {
+      res.status(200).json(data);
+    }
+  });
+};
+
+//Use model.find() to Search Your Database
+exports.findPerson = (req, res, next) => {
+  const info = req.params.name;
+  Person.find({ name: info }, (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
+
+//Use model.findById() to Search Your Database By _id
+
+exports.findById = (req, res, next) => {
+  const info = req.body.personId;
+  Person.findById(info, (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
+
+//Use model.findOne() to Return a Single Matching Document from Your Database
+exports.findOne = (req, res, next) => {
+  const searchItem = req.body.food;
+  Person.findOne({ favoritefood: { $all: searchItem} }, (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 };
--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
-const postController = require('../controllers/post')
+const postController = require("../controllers/post");
 
-router.get('/', (req, res, next) => {
-    res.status(200).send("Welcome to person's profile")
-})
+const Person = require("../models/Person");
 
-router.post('/', postController.postInfoPerson)
+router.get("/", async (req, res, next) => {
+  // res.status(200).send("Welcome to person's profile")
+  try {
+    const result = await Person.find();
+    res.status(200).json(result)
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+});
 
-module.exports = router
+router.post("/", postController.postInfoPerson);
+
+router.get("/people", postController.getInfoPeople);
+
+router.post("/people", postController.postInfoPeople);
+
+router.post("/findbyid", postController.findById);
+
+router.post("/findone", postController.findOne);
+
+router.get("/:name", postController.findPerson);
+
+module.exports = router;
